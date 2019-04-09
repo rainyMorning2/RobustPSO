e75f89176f2a76de321ca077d40425946a32c72c
Zhen Dai <rainyMorning1998@gmail.com>
2019-04-09 18:25:52 +0800

    Update MObenchmarks.py
    add the Kursawe function

--- MObenchmarks.py
+++ MObenchmarks.py
@@ -1,6 +1,30 @@
 import numpy as np
 
 
+class f2(object):
+    low = np.array([-5, -5, -5])
+    high = np.array([5, 5, 5])
+
+    def __init__(self, N, D=3, K=2):
+        self.N = N  # number of particles
+        self.D = D  # dimensions of vector x
+        self.K = K  # dimensions of vector f(x)
+
+    def getValue(self, position):
+
+        result = np.zeros([self.N, self.K])
+        cnt = np.zeros(self.N)
+
+        for i in range(0, self.N):
+
+            result[i][0] = -10 * np.e ** (-0.2 * (position[i][0] ** 2 + position[i][1] ** 2) ** 0.5) \
+                           - 10 * np.e ** (-0.2 * (position[i][1] ** 2 + position[i][2] ** 2) ** 0.5)
+            for j in range(0, self.D):
+                result[i][1] += abs(position[i][j]) ** 0.8 + 5 * np.sin(position[i][j] ** 3)
+
+        return result, cnt
+
+
 class f1(object):
     low = np.array([0, 0])
     high = np.array([7, 7])
